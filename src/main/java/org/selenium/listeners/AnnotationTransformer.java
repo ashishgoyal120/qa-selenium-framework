ffa4ee6b043c007006e8fb083bcaf4a13e673dc8
Ashish-Mac <ashishg120@gmail.com>
2025-08-29 20:39:29 +0530

    Updated readme

--- src/main/java/org/selenium/listeners/AnnotationTransformer.java
+++ src/main/java/org/selenium/listeners/AnnotationTransformer.java
@@ -9,8 +9,26 @@ import org.testng.annotations.ITestAnnotation;
 public class AnnotationTransformer implements IAnnotationTransformer{
 
 	@Override
+	@SuppressWarnings("rawtypes")
 	public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) {
 		annotation.setRetryAnalyzer(RetryFailedTests.class);
+		// Usage of other parameters
+		/*
+		 * // 🚫 Example 1: Skip all tests inside "PaymentTests" class
+		 * if (testClass != null && testClass.getSimpleName().equals("PaymentTests")) {
+		 * annotation.setEnabled(false);
+		 * }
+		 * 
+		 * // 🚫 Example 2: Skip specific method "testLogin" (regardless of class)
+		 * if (testMethod != null && testMethod.getName().equals("testLogin")) {
+		 * annotation.setEnabled(false);
+		 * }
+		 * 
+		 * // ✅ Example 3: Retry only for "testCheckout" method
+		 * if (testMethod != null && testMethod.getName().equals("testCheckout")) {
+		 * annotation.setRetryAnalyzer(RetryFailedTests.class);
+		 * }
+		 */
 	}
 
 }
